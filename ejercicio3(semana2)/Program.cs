41daa39a0fee8bdf205c5778ee9b810ffb28e52a
Guess2007 <enzogamer70yt@gmail.com>
2025-08-31 18:25:46 -0500

    ,

---
+++ ejercicio3(semana2)/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio3_semana2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            int mayor;
+            int menor;
+
+
+            do
+            {
+                Console.WriteLine("Ingrese el primer numero:");
+                num1 = int.Parse(Console.ReadLine());
+                if (num1 < 0)
+                {
+                    Console.WriteLine("El numero no puede ser negativo, intente de nuevo.");
+                    continue;
+                }
+                else if (num1 == 0)
+                {
+                    Console.WriteLine("El numero no puede ser cero, intente de nuevo.");
+                    continue;
+                }
+                else if (!int.TryParse(num1.ToString(), out num1))
+                {
+                    Console.WriteLine("Entrada invalida, intente de nuevo.");
+                    continue;
+
+                }
+
+                do
+                {
+                    Console.WriteLine("Ingrese el segundo numero:");
+                    num2 = int.Parse(Console.ReadLine());
+                    if (num2 < 0)
+                    {
+                        Console.WriteLine("El numero no puede ser negativo, intente de nuevo.");
+                        continue;
+                    }
+                    else if (num2 == 0)
+                    {
+                        Console.WriteLine("El numero no puede ser cero, intente de nuevo.");
+                        continue;
+                    }
+                    else if (!int.TryParse(num2.ToString(), out num2))
+                    {
+                        Console.WriteLine("Entrada invalida, intente de nuevo.");
+                        continue;
+                    }
+                } while (num2 < 0);
+                do
+                {
+                    Console.WriteLine("Ingrese el tercer numero:");
+                    num3 = int.Parse(Console.ReadLine());
+                    if (num3 < 0)
+                    {
+                        Console.WriteLine("El numero no puede ser negativo, intente de nuevo.");
+                        continue;
+                    }
+                    else if (num3 == 0)
+                    {
+                        Console.WriteLine("El numero no puede ser cero, intente de nuevo.");
+                        continue;
+                    }
+                    else if (!int.TryParse(num3.ToString(), out num3))
+                    {
+                        Console.WriteLine("Entrada invalida, intente de nuevo.");
+                        continue;
+                    }
+                } while (num3 < 0);
+
+            } while (num1 < 0);
+            mayor = Math.Max(num1, Math.Max(val1: num2, val2: num3));
+            menor = Math.Min(num1, Math.Min(num2, num3));
+            Console.WriteLine("El numero mayor es: " + mayor);
+            Console.WriteLine("El numero menor es: " + menor);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tres en uno/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero1 = 0;
+            double numero2 = 0;
+            double numero3 = 0;
+            bool correcto;
+
+
+            do
+            {
+                Console.Write("Ingrese el primer número: ");
+                string entrada1 = Console.ReadLine();
+                correcto = double.TryParse(entrada1, out numero1);
+
+                if (!correcto)
+                {
+                    Console.WriteLine("Entrada inválida. Por favor, ingrese un número válido.");
+                }
+            } while (!correcto);
+
+
+            do
+            {
+                Console.Write("Ingrese el segundo número: ");
+                string entrada2 = Console.ReadLine();
+                correcto = double.TryParse(entrada2, out numero2);
+
+                if (!correcto)
+                {
+                    Console.WriteLine("Entrada inválida. Por favor, ingrese un número válido .");
+                }
+            } while (!correcto);
+
+
+            do
+            {
+                Console.Write("Ingrese el tercer número: ");
+                string entrada3 = Console.ReadLine();
+                correcto = double.TryParse(entrada3, out numero3);
+
+                if (!correcto)
+                {
+                    Console.WriteLine(" Entrada inválida. Por favor, ingrese un número válido .");
+                }
+            } while (!correcto);
+
+
+            double intermedio;
+
+            bool esIntermedio1 = (numero1 > numero2 && numero1 < numero3) || (numero1 < numero2 && numero1 > numero3);
+            bool esIntermedio2 = (numero2 > numero1 && numero2 < numero3) || (numero2 < numero1 && numero2 > numero3);
+            bool esIntermedio3 = (numero3 > numero1 && numero3 < numero2) || (numero3 < numero1 && numero3 > numero2);
+
+            if (esIntermedio1)
+            {
+                intermedio = numero1;
+            }
+            else if (esIntermedio2)
+            {
+                intermedio = numero2;
+            }
+            else
+            {
+                intermedio = numero3;
+            }
+
+            double mayor;
+
+            if (numero1 >= numero2 && numero1 >= numero3)
+            {
+                mayor = numero1;
+            }
+            else if (numero2 >= numero1 && numero2 >= numero3)
+            {
+                mayor = numero2;
+            }
+            else
+            {
+                mayor = numero3;
+            }
+
+            double menor;
+
+            if (numero1 <= numero2 && numero1 <= numero3)
+            {
+                menor = numero1;
+            }
+            else if (numero2 <= numero1 && numero2 <= numero3)
+            {
+                menor = numero2;
+            }
+            else
+            {
+                menor = numero3;
+            }
+
+            Console.WriteLine("El número mayor es: " + mayor);
+            Console.WriteLine("El número menor es: " + menor);
+            Console.WriteLine("El número intermedio es: " + intermedio);
+
+        }
+    }
+}
