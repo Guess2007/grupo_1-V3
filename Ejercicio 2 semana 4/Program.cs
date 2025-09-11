874e9f3dea0c81129abe94ac203a56db12d754d1
Guess2007 <enzogamer70yt@gmail.com>
2025-09-11 00:57:51 -0500

    adelantos de esta semana

---
+++ Ejercicio 2 semana 4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2_semana_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, dec, uni, cen;
+            string linea;
+
+            Console.WriteLine("ingrese un numero de tres digitos: "); linea = Console.ReadLine();
+            num = int.Parse(linea);
+
+            dec = num / 100;
+            cen = (num / 10) % 10;
+            uni = num % 10;
+            int aux = (uni * 100) + (cen * 10) + dec;
+
+            Console.WriteLine("el numero invertido es: " + aux);
+            Console.WriteLine("pulse una tecla: "); Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ ejercicio 3 semana 4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_3_semana_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2;
+            string operacion;
+            Console.WriteLine("Ingrese el primer numero");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la operacion a realizar (+, -, *, /)");
+            operacion = Console.ReadLine();
+
+            switch (operacion)
+            {
+                case "+":
+                    Console.WriteLine("El resultado de la suma es: " + (num1 + num2));
+                    break;
+                case "-":
+                    Console.WriteLine("El resultado de la resta es: " + (num1 - num2));
+                    break;
+                case "*":
+                    Console.WriteLine("El resultado de la multiplicacion es: " + (num1 * num2));
+                    break;
+                case "/":
+                    if (num2 != 0)
+                    {
+                        Console.WriteLine("El resultado de la division es: " + (num1 / num2));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: No se puede dividir por cero.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Operacion no valida.");
+                    break;
+            }
+        }
+    }
+}
