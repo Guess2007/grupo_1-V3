4f257217c33ff05deb8a9b6213267095ae168569
Mdro123 <134395516+Mdro123@users.noreply.github.com>
2025-09-18 13:57:52 -0500

    .

--- Semana 5 LCOM/Program.cs
+++ Semana 5 LCOM/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,45 +9,10 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args, int tipo)
         {
-            /*
-            for (int i = 40; i <= 100; i ++) 
-            {
-                Console.WriteLine(i);
-            }
-            Console.WriteLine("Pulse para finalizar");
-            Console.ReadLine();
-            */
-
-            /*
-            byte num, i;
-            int RESULT;
-            string linea;
-
-            Console.WriteLine("Ingrese un numero entero: "); linea = Console.ReadLine();
-            num = byte.Parse(linea);
-            Console.WriteLine();
-
-            for (i = 100; i >= 1; i--)
-            {
-                RESULT = num * i;
-                Console.WriteLine(num + "*" + i + "=" + RESULT);
-            }
-            Console.WriteLine("Gracias por usar el programa Csharp");
-            Console.ReadKey();
-            */
-
-            byte num, I;
-            int result, kind;
-            string linea;
-
-            Console.WriteLine("Ingrese un numero entero"); linea = Console.ReadLine();
-            num = byte.Parse(linea);
-            Console.WriteLine();
-
-            switch (kind):
-                case "par":
+            
+                
 
         }
     }
