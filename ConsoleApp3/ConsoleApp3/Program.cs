6f11da4e36ae8a6af713a757c3bc8681fb72659b
Guess2007 <enzogamer70yt@gmail.com>
2025-09-23 10:37:40 -0500

    .

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int producto, num1, num2, num3;
+            string linea;
+
+            Console.WriteLine("bienvenido, ingrese su pedido"); linea = Console.ReadLine();
+            producto = int.Parse(linea);
+
+            if (producto == 1)
+            {
+                Console.WriteLine("usted ha elegido comida, seleccione el producto que desea"); linea = Console.ReadLine();
+                producto = int.Parse(linea);
+                switch (producto)
+                {
+                    case 1:
+                        Console.WriteLine("usted ha elegido PAN \n precio: 3.50");
+                        Console.WriteLine("¿Desea continuar con su compra? s/n"); linea = Console.ReadLine();
+                        
+                        if (linea == "s")
+                        {
+                            Console.WriteLine("compra realizada exitosamente, tenga buen dia");
+                            Console.ReadKey();
+                        }
+                        else if (linea == "n")
+                        {
+                            Console.WriteLine("compra cancelada...");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine("comando invalido-");
+                        }
+                        break;
+                        case 2:
+                        Console.WriteLine("usted ha elegido tallarines verdes \n precio: 5.00");
+                        Console.WriteLine("¿Desea continuar con su compra? s/n"); linea = Console.ReadLine();
+                        
+                        if (linea == "s")
+                        {
+                            Console.WriteLine("compra realizada exitosamente, tenga buen dia");
+                            Console.ReadKey();
+                        }
+                        else if (linea == "n")
+                        {
+                            Console.WriteLine("compra cancelada...");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine("comando invalido-");
+                        }
+                        break;
+                        case 3:
+                        Console.WriteLine("usted ha elegido aji de gallina \n precio: 4.50");
+                        Console.WriteLine("¿Desea continuar con su compra? s/n"); linea = Console.ReadLine();
+                        
+                        if (linea == "s")
+                        {
+                            Console.WriteLine("compra realizada exitosamente, tenga buen dia");
+                            Console.ReadKey();
+                        }
+                        else if (linea == "n")
+                        {
+                            Console.WriteLine("compra cancelada...");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine("comando invalido-");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("comando invalido");
+                        break;
+                }
+            }
+            else if (producto == 2)
+            {
+                Console.WriteLine("usted ha elegido bebida, seleccione el producto que desea"); linea = Console.ReadLine();
+                producto = int.Parse(linea);
+                switch (producto)
+                {
+                    case 1:
+                        Console.WriteLine("usted ha elegido CHICHA \n precio: 2.50");
+                        Console.WriteLine("¿Desea continuar con su compra? s/n"); linea = Console.ReadLine();
+                        
+                        if (linea == "s")
+                        {
+                            Console.WriteLine("compra realizada exitosamente, tenga buen dia");
+                            Console.ReadKey();
+                        }
+                        else if (linea == "n")
+                        {
+                            Console.WriteLine("compra cancelada...");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine("comando invalido-");
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("usted ha elegido INCA COLA \n precio: 6.00");
+                        Console.WriteLine("¿Desea continuar con su compra? s/n"); linea = Console.ReadLine();
+                        
+                        if (linea == "s")
+                        {
+                            Console.WriteLine("compra realizada exitosamente, tenga buen dia");
+                            Console.ReadKey();
+                        }
+                        else if (linea == "n")
+                        {
+                            Console.WriteLine("compra cancelada...");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine("comando invalido-");
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("usted ha elegido PEPSI \n precio: 2.50");
+                        Console.WriteLine("¿Desea continuar con su compra? s/n"); linea = Console.ReadLine();
+                        
+                        if (linea == "s")
+                        {
+                            Console.WriteLine("compra realizada exitosamente, tenga buen dia");
+                            Console.ReadKey();
+                        }
+                        else if (linea == "n")
+                        {
+                            Console.WriteLine("compra cancelada...");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine("comando invalido-");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("comando invalido");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("comando invalido, intente de nuevo");
+            }
+                
+        }
+    }
+}
