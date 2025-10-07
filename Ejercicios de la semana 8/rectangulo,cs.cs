41d1ee0a7225bef580918cd5caf261639320f820
Guess2007 <enzogamer70yt@gmail.com>
2025-10-07 11:40:14 -0500

    .

---
+++ Ejercicios de la semana 8/rectangulo,cs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios_de_la_semana_8
+{
+    internal class rectangulo_cs
+    {
+    }
+}
