581e9f47bb49328f6ae7cb029025b588f675bf4d
Guess2007 <enzogamer70yt@gmail.com>
2025-10-07 11:41:07 -0500

    .

---
+++ Clase de la semana 7 (fuera del main)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_de_la_semana_7__fuera_del_main_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dat1 = 0;
+            double dat2 = 0;
+
+            Console.WriteLine("Ingrese la base y la altura del rectangulo: ");
+            Console.Write("INGRESE LA BASE: ");
+            dat1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("INGRESE LA ALTURA: ");
+            dat2 = Convert.ToDouble(Console.ReadLine());
+
+            Rectangulo rectangulo = new Rectangulo(dat1, dat2);
+            rectangulo.calcularArea();
+            double perimetro = rectangulo.calcularPerimetro();
+
+            Console.WriteLine("El perimetro del rectangulo es: " + perimetro);
+            double area = rectangulo.calcularArea2(3, 6);
+            Console.WriteLine("El area 2 del rectangulo es: " + area);
+        }
+    }
+}
