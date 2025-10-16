d8d14b530293a7c13b5c5cdd96aeb010b8f94f7b
Mdro123 <134395516+Mdro123@users.noreply.github.com>
2025-10-16 13:32:34 -0500

    .

---
+++ BIBLIOTECAABC/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace BIBLIOTECAABC
+{
+    public class Class1
+    {
+        public static int sumar(int x, int y) 
+        {
+            return x + y;
+        }
+        public static int restar(int x, int y)
+        {
+            return x - y;
+        }
+        public static int dividir(int x, int y)
+        {
+            return x / y;
+        }
+        public static int multiplicar(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
