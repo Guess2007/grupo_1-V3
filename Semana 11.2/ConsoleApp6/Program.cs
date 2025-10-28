62134d99cee98d918d6b17337d973a3d0c69defe
Guess2007 <enzogamer70yt@gmail.com>
2025-10-28 10:00:37 -0500

    .

---
+++ Semana 11.2/ConsoleApp6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            string ent;
+
+            Console.Write(" ingresar el tamaño del array: ");
+            ent = Console.ReadLine(); num = int.Parse(ent);
+
+            int[] notas = new int[num];
+
+            Console.WriteLine("el tamaño de NOTAS es :" + notas.Length);
+
+            for (int i = 0; i < num; i++)
+            {
+                string ent1 = "";
+                int num1 = 0;
+                Console.Write("ingresa el valor de la casilla: ");
+                ent1 = Console.ReadLine(); num1 = int.Parse(ent1);
+                notas[i] = num1;
+
+            }
+
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("el valor de la casilla " + i + " es: " + notas[i]);
+
+            }
+
+        }
+    }
+}
