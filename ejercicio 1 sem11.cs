726e3c292b1f666251001d95e457405eccbb8a05
Mdro123 <134395516+Mdro123@users.noreply.github.com>
2025-10-30 13:28:24 -0500

    update

---
+++ ejercicio 1 sem11.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int CAN = 0;
+            int POS = 0;
+            string linea;
+            Random rnd = new Random();
+            
+            Console.Write("Numero de aleatorios :");
+            linea = Console.ReadLine();  CAN = int.Parse(linea);
+
+            double[] VEC = new double[CAN + 1];
+            for (POS = 1; POS <= CAN; POS++)
+            {
+                VEC[POS] = rnd.Next(0, 99);
+               
+            }
+           
+            for (POS = 1; POS <= CAN; POS++)
+            {
+                
+                Console.WriteLine(VEC[POS]);
+
+            }
+
+
+
+
+        }
+    }
+}
---
+++ ejercicio 2 sem11.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] caracteres;
+            string entrada1 = "";
+            int num1 = 0;
+
+            Console.Write("ingrese el texto :  ");
+            entrada1 = Console.ReadLine(); 
+            caracteres = entrada1.ToCharArray();
+            num1 = caracteres.Length;
+            for (int i = 0; i < num1; i++)
+            {
+                Console.WriteLine( caracteres[i]);
+
+            }
+        }
+    }
+}
