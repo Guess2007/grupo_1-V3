71df1fb1834c54b6684973936d4da524fd745a61
Mdro123 <134395516+Mdro123@users.noreply.github.com>
2025-11-06 13:06:24 -0500

    .

---
+++ LCOM2 1.1 (Semana 12)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCOM2_1._1__Semana_12_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int aux;
+
+            Console.WriteLine("¿Cuantos elementos? (maximo: 12) ");
+            aux = int.Parse(Console.ReadLine());
+
+            int[] lista = new int[aux];
+
+            for (int i = 0; i < aux; i++)
+            {
+                int num = 0;
+                Console.WriteLine("posicion " + i + ": ");
+                num = int.Parse(Console.ReadLine());
+                lista [i] = num;
+
+                Console.WriteLine(lista[i]);
+            }
+
+            
+        }
+    }
+}
