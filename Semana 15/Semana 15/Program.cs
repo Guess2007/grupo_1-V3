f5b078b87e258b373d3afa4f2ecb863f68cd4143
Guess2007 <enzogamer70yt@gmail.com>
2025-11-25 10:14:38 -0500

    .

--- Semana 15/Semana 15/Program.cs
+++ Semana 15/Semana 15/Program.cs
@@ -10,7 +10,7 @@ namespace Semana_15
     {
         static void Main(string[] args)
         {
-            matriz1();
+            matriz2();
         }
         public static void matriz0() 
         { 
@@ -21,17 +21,28 @@ namespace Semana_15
                 columna = int.Parse(Console.ReadLine());
                 Console.Write("ingrese el numero de filas: ");
                 fila = int.Parse(Console.ReadLine());
-
-                a = new int[fila, columna];
+                //Paso 1: la consola pide al usuario las caracteristicas del array.
+                a = new int[fila, columna];//array que almacena los datos ingresados por el usuario.
                 Console.WriteLine("\ningrese los valores de su matriz: ");
                 for (int i = 0; i < fila; i++)
                 {
-                    for (int j = 0; j < columna; j++)
+                    for (int j = 0; j < columna; j++)//Tanto el for i como el for j se encargaran de hacer un bucle para que el usuario vaya llenando los espacios del array hasta que i sea igual al numero columnas y j el numero de filas
                     {
                         Console.Write("a[{0},{1}]: ", i, j);
-                        a[i, j] = int.Parse(Console.ReadLine());
+                        a[i, j] = int.Parse(Console.ReadLine());//Mientras que este ira almacenando lo que vaya escribiendo el usuario en el bucle
                     }
                 }
+                Console.Write("*Matriz a[{0},{1}]: \n", fila, columna);
+                //Escribir la matriz actual
+                for (int i = 0; i < fila; i++) 
+                {
+                    for (int j = 0;j < columna; j++) 
+                    {
+                        Console.Write("[{0}]  ", a[i,j]);//En comparacion al for anterior, este for se encargara de imprimir la matriz hasta que, nuevamente, i iguale el numero de filas, y j el numero de columnas
+                    }
+                    Console.WriteLine();
+                }
+                Console.ReadKey();
             } 
         }
         public static void matriz1() 
@@ -40,14 +51,14 @@ namespace Semana_15
                 int[,] a = { { 1, 2, 3},
                              { 4, 5, 6},
                              { 7, 8, 9}};
-                int t_f = a.GetLength(0);
-                int t_c = a.GetLength(1);
+                int t_f = a.GetLength(0);//operador encargado de obtener el largo de las filas
+                int t_c = a.GetLength(1);//operador encargado de obtener el largo de las columnnas
                 Console.WriteLine("\n* Matriz a[{0},{1}]: ", t_f, t_c);
-                for (int i = 0; i < t_f; i++)
+                for (int i = 0; i < t_f; i++)//bucle encargado de que el .write escriba las filas de la matriz hasta llegar a su limite
                 {
-                    for (int j = 0; j < t_c; j++)
+                    for (int j = 0; j < t_c; j++)//bucle encargado de que el .write escriba las columnas de la matriz hasta llegar a su limite
                     {
-                        Console.Write("[{0}] ", a[i, j]);
+                        Console.Write("[{0}] ", a[i, j]);//.Write encargado de escribir la matriz inicial en base al bucle for de i y j
                     }
                     Console.WriteLine();
                 }
@@ -56,10 +67,9 @@ namespace Semana_15
                 {
                     for (int j = 0; j < t_c; j++)
                     {
-                        if (i == j)
+                        if (i == j)//if que sirve para que el bucle for solo escriba las ubicaciones de misma ubicacion en fila y columna
                         {
-                            Console.WriteLine("a[{0},{1}] = {2}",
-                            i, j, a[i, j]);
+                            Console.WriteLine("a[{0},{1}] = {2}", i, j, a[i, j]);
                         }
                     }
                 }
@@ -69,6 +79,26 @@ namespace Semana_15
         public static void matriz2() 
         { 
             {
+                int[,] a = { { 1, 2, 3 },
+                             { 4, 5, 6 },
+                             { 7, 8, 9 }};
+                int t_f = a.GetLength(0);//operador encargado de obtener el largo de las filas
+                int t_c = a.GetLength(1);//operador encargado de obtener el largo de las columnnas
+                Console.WriteLine("\n* Matriz a[{0},{1}]: ", t_f, t_c);
+                for (int i = 0; i < t_f; i++)//bucle encargado de que el .write escriba las filas de la matriz hasta llegar a su limite
+                {
+                    for (int j = 0; j < t_c; j++)//bucle encargado de que el .write escriba las columnas de la matriz hasta llegar a su limite
+                    {
+                        Console.Write("[{0}] ", a[i, j]);//.Write encargado de escribir la matriz inicial en base al bucle for de i y j
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("\n* Diagonal inversa:");
+                for (int i = 0; i < t_f; i++)
+                {
+                    Console.WriteLine("a[{0},{1}]={2}",i, t_f - i - 1, a[i, t_f - i - 1]);//en el caso de estas dos formulas, estas nos sirven para poder ajustar lo que se va a mostrar en el array
+                }
+                Console.ReadKey();
             } 
         }
     }
